31292e31f3f15f3f96d8d60d4a43a67ffe702a8c
ClownArchipel <97152132+ClownArchipel@users.noreply.github.com>
2022-06-14 22:36:15 +0700

    Uploading file
    try to make calculator

---
+++ script.js
@@ -0,0 +1,103 @@
+let prevNumber='';
+let calculationOperator='';
+let currentNumber='';
+
+const inputNumber=(number)=>{
+    if(currentNumber==='0'){
+        currentNumber=number;
+    }else{
+        currentNumber+=number;
+    }
+}
+
+const calculatorScreen=document.querySelector('.calculator-screen');
+
+const updateScreen=(number)=>{
+    calculatorScreen.value=number;
+}
+
+const numbers=document.querySelectorAll(".number");
+
+numbers.forEach((number)=>{
+    number.addEventListener("click",(event)=>{
+        inputNumber(event.target.value);
+        updateScreen(currentNumber);
+    });
+})
+
+const inputOperator=(operator=>{
+    if(calculationOperator===''){
+        prevNumber=currentNumber;
+    }
+    calculationOperator = operator;
+    currentNumber = '0'
+});
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach((operator)=>{
+    operator.addEventListener("click",(event)=>{
+        console.log(event.target.value);
+    });
+});
+operators.forEach((operator)=>{
+    operator.addEventListener("click",(event)=>{
+        inputOperator(event.target.value);
+    });
+});
+
+const equalsign = document.querySelector('.equal-sign');
+
+equalsign.addEventListener('click',()=>{
+   calculate();
+   updateScreen(currentNumber);
+});
+
+const calculate = ()=>{
+    let result = '';
+    switch(calculationOperator){
+        case "+":
+            result = parseFloat(prevNumber) + parseFloat(currentNumber);
+            break;
+        case "-":
+            result = parseFloat(prevNumber) - parseFloat(currentNumber);
+            break;
+        case "*":
+            result = parseFloat(prevNumber) * parseFloat(currentNumber);
+            break;
+        case "/":
+            result = parseFloat(prevNumber) / parseFloat(currentNumber);
+            break;
+        case "%":
+            result = parseFloat(prevNumber) % parseFloat(currentNumber);
+            console.log(result);
+            break;
+        default:
+            break;
+    }
+    currentNumber = result;
+    calculationOperator = '';
+}
+
+const clearBtn = document.querySelector('.all-clear');
+const clearAll=()=>{
+    prevNumber='';
+    calculationOperator='';
+    currentNumber='0';
+}
+
+clearBtn.addEventListener('click',()=>{
+    clearAll();
+    updateScreen(currentNumber);
+});
+
+const decimal = document.querySelector('.decimal');
+const inputDecimal =(dot)=>{
+    if(currentNumber.includes('.')){
+        return
+    }
+    currentNumber +=dot;
+}
+decimal.addEventListener('click',(event)=>{
+    inputDecimal(event.target.value);
+    updateScreen(currentNumber);
+});
